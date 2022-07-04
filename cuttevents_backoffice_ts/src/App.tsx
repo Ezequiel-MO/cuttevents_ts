9337e9792875fbcb8110124c219e482860aeb583
Ezequiel <oliverm316@gmail.com>
2022-07-04 09:48:57 +0200

    Project kick off

---
+++ cuttevents_backoffice_ts/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      CUTT/events backoffice in Typescript, using TDD, and styled components
+      with tailwindcss
+    </div>
+  );
+}
+
+export default App;
